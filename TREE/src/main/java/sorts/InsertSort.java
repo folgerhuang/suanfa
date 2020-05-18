26b2b02b5c736155031a4bb55d28fdb98a974c77
folgerhuang <379771379@qq.com>
2020-05-18 19:04:56 +0800

    sort alg

---
+++ TREE/src/main/java/sorts/InsertSort.java
@@ -0,0 +1,53 @@
+package sorts;
+
+import com.google.common.collect.Lists;
+
+import java.net.Inet4Address;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class InsertSort {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> objects = new ArrayList<>();
+        while (!scanner.hasNext("end")) {
+            objects.add(scanner.nextInt());
+        }
+
+        System.out.println("Before sort:\n");
+
+        for (int i = 0; i < objects.size(); i++) {
+            System.out.print(objects.get(i) + " ");
+        }
+        System.out.println("after sort:\n");
+        Integer[] objects1 = (Integer[]) objects.toArray();
+        Integer[] ints = insertSort(objects1);
+        for (int i = 0; i < ints.length; i++) {
+            System.out.println(ints[i] + " ");
+        }
+        System.out.println("\n");
+    }
+
+    public static Integer[]  insertSort(Integer[] a) {
+        if (a == null || a.length == 0) {
+            return a;
+        }
+
+        for (int i = 1; i < a.length; i++) {
+            int j = i - 1;
+            int insertData = a[i];
+            while (j >= 0 && a[j] > insertData) {
+                a[j + 1] = a[j];
+                j--;
+            }
+            a[j + 1] = insertData;
+        }
+
+        return a;
+
+
+
+    }
+}
+
+
